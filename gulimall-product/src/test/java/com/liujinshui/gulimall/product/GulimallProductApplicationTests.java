dbde3cf650704c9e2fd18aa39a2b4e2495b04602
rainoprime <it@liujinshui.com>
2021-11-14 16:37:43 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/liujinshui/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liujinshui.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
