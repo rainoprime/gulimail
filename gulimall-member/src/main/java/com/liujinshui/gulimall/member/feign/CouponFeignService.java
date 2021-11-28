a7e08de2f1fa27d4d11813bab7f045e5becb1070
rainoprime <it@liujinshui.com>
2021-11-28 17:17:38 +0800

    项目初始结构创建

---
+++ gulimall-member/src/main/java/com/liujinshui/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,12 @@
+package com.liujinshui.gulimall.member.feign;
+
+import com.liujinshui.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R membercoupons();
+}
